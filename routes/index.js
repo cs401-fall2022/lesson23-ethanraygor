5c24f442620a8c4cab333cfc7922d339b950bc84
ethanraygor <89420452+ethanraygor@users.noreply.github.com>
2022-11-14 10:29:57 -0700

    express basics

---
+++ routes/index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+const sqlite3 = require('sqlite3').verbose()
+const { numberOnly } = require('../public/javascripts/helpers.js');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      //Query if the table exists if not lets create it on the fly!
+      db.all(`SELECT name FROM sqlite_master WHERE type='table' AND name='blog'`,
+        (err, rows) => {
+          if (rows.length === 1) {
+            console.log("Table exists!");
+            db.all(` select blog_id, blog_txt from blog`, (err, rows) => {
+              console.log("returning " + rows.length + " records");
+              res.render('index', { title: 'Express', data: rows });
+            });
+          } else {
+            console.log("Creating table and inserting some sample data");
+            db.exec(`create table blog (
+                     blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     blog_txt text NOT NULL);
+                      insert into blog (blog_txt)
+                      values ('This is a great blog'),
+                             ('Oh my goodness blogging is fun');`,
+              () => {
+                db.all(` select blog_id, blog_txt from blog`, (err, rows) => {
+                  res.render('index', { title: 'Express', data: rows });
+                });
+              });
+          }
+        });
+    });
+});
+
+router.post('/add', (req, res, next) => {
+  console.log("Adding blog to table without sanitizing input! YOLO BABY!!");
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      console.log("inserting " + req.body.blog);
+      //This seems safe to insert because all of it will be included in the one string as a blog post
+      db.exec(`insert into blog ( blog_txt)
+                values ('${req.body.blog}');`)
+      //redirect to homepage
+      res.redirect('/');
+    }
+  );
+})
+
+router.post('/delete', (req, res, next) => {
+  console.log("deleting stuff without checking if it is valid! SEND IT!");
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      console.log("sanitizing " + req.body.blog);
+      var str = numberOnly(req.body.blog);
+      console.log("deleting " + str);
+      db.exec(`delete from blog where blog_id='${str}';`);     
+      res.redirect('/');
+    }
+  );
+})
+
+module.exports = router;
